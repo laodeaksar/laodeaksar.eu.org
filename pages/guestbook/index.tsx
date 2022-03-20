7a1de2efd976c0eb9ff29960614779157183b26b
laodeaksarr <akhsarodhe@gmail.com>
2022-03-20 13:10:38 +0800

    fix error

--- pages/guestbook/index.tsx
+++ pages/guestbook/index.tsx
@@ -128,6 +128,7 @@ function GuestbookForm() {
 
 function LogInWithGithub() {
   return (
+    // eslint-disable-next-line @next/next/no-html-link-for-pages
     <a
       href="/api/auth/signin/github"
       onClick={(e) => {
@@ -191,11 +192,11 @@ function Entry({ entry }: { entry: GuestbookEntry }) {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth="2"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                     />
                   </svg>
