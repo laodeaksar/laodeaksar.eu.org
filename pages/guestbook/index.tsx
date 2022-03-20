bced5ce6740a2138452a546e2f4effc6de078ac4
laodeaksarr <akhsarodhe@gmail.com>
2022-03-20 13:04:52 +0800

    add icon and Toaster

--- pages/guestbook/index.tsx
+++ pages/guestbook/index.tsx
@@ -183,9 +183,25 @@ function Entry({ entry }: { entry: GuestbookEntry }) {
           {data?.user?.email === entry.email && (
             <>
               <span>&bull;</span>
-              <Button variant="primary" onClick={handleDelete}>
-                Delete
-              </Button>
+              <Button
+                variant="icon"
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                }
+                onClick={handleDelete}
+              />
             </>
           )}
         </Grid>
@@ -217,23 +233,24 @@ const GuestbookPage = ({ fallback }) => (
           information, wisdom, or even humor. Surprise me!
         </Text>
         <Card>
-        <Card.Header css={{ fontSize: '$5' }}>Sign the Guestbook</Card.Header>
-        <Card.Body>
-          <Text
-            as="p"
-            css={{
-              marginTop: '0px',
-              marginBottom: '$2'
-            }}
-          >
-            Share a message for a future visitor of my site.
-          </Text>
-          <GuestbookBody/>
-        </Card.Body>
-      </Card>
+          <Card.Header css={{ fontSize: '$5' }}>Sign the Guestbook</Card.Header>
+          <Card.Body>
+            <Text
+              as="p"
+              css={{
+                marginTop: '0px',
+                marginBottom: '$2'
+              }}
+            >
+              Share a message for a future visitor of my site.
+            </Text>
+            <GuestbookBody />
+          </Card.Body>
+        </Card>
         <SWRConfig value={{ fallback }}>
           <GuestbookEntries />
         </SWRConfig>
+        <Toaster />
       </div>
     </Grid>
   </Layout>
