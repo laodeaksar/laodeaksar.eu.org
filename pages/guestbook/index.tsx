a02686f1e23411e4459c1ddc1f70d6225757581f
laodeaksarr <akhsarodhe@gmail.com>
2022-03-23 19:16:47 +0800

    edit

--- pages/guestbook/index.tsx
+++ pages/guestbook/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import useSWR, { SWRConfig, useSWRConfig } from 'swr';
 import toast, { Toaster } from 'react-hot-toast';
 import { useForm, SubmitHandler, useWatch } from 'react-hook-form';
@@ -188,7 +188,7 @@ function Entry({ entry }: { entry: GuestbookEntry }) {
         {entry.created_by}
       </Text>
       <Flex>{entry.body}</Flex>
-      <Grid gapX={2} flow="column" align="center" justify='between' mt={3}>
+      <Grid gapX={2} flow="column" align="center" justify="between" mt={3}>
         <time dateTime={entry.updated_at}>
           {new Date(entry.updated_at).toLocaleDateString('en', {
             month: 'short',
@@ -244,31 +244,35 @@ function Entry({ entry }: { entry: GuestbookEntry }) {
 
 function GuestbookEntries() {
   const { data: entries } = useSWR<GuestbookEntry[]>('/api/guestbook', fetcher);
+  const { data: session } = useSession();
 
   return (
     <Card css={{ marginTop: '$4' }}>
       <Card.Header css={{ fontSize: '$3' }}>
         All Message
-        <Button
-          variant="icon"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-width:'24'
-height:'24'
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              />
-            </svg>
-          }
-        />
+        {session && (
+          <Button
+            variant="icon"
+            onClick={() => signOut()}
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+            }
+          />
+        )}
       </Card.Header>
       <Card.Body>
         {entries?.map((entry) => (
