94830076c3e640ed56d4c13a6d621a7f598d83f3
bahutara <akhsaras23@gmail.com>
2022-07-11 18:30:50 +0000

    esit

--- core/layout/types.ts
+++ core/layout/types.ts
@@ -1,6 +1,7 @@
 import type { HeaderProps } from '~/components/Header';
 
 export interface LayoutProps {
+  children?:React.ReactNode;
   footer?: boolean;
   header?: boolean;
   headerProps?: HeaderProps;
