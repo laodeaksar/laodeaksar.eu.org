f989217273ea1f9a48785ef21f6e8792b4ed7385
bahutara <108091324+bahutara@users.noreply.github.com>
2022-07-15 20:49:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// const { withContentlayer } = require('next-contentlayer');
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
@@ -25,7 +24,8 @@ module.exports = withPWA({
   },
   experimental: {
     legacyBrowsers: false,
-    browsersListForSwc: true
+    browsersListForSwc: true,
+    images: { allowFutureImage: true }
   },
   pwa: {
     dest: "public",
@@ -52,16 +52,67 @@ module.exports = withPWA({
       }
     ];
   },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    // if (!dev && !isServer) {
-    //   Object.assign(config.resolve.alias, {
-    //     'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-    //     react: 'preact/compat',
-    //     'react-dom/test-utils': 'preact/test-utils',
-    //     'react-dom': 'preact/compat'
-    //   });
-    // }
-    return config;
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ];
   }
 });
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.google-analystics.com *.codesandbox.io *.splitbee.io;
+    child-src *.youtube.com *.google.com *.twitter.com *.codesandbox.io *.splitbee.io;
+    style-src 'self' 'unsafe-inline' 'unsafe-eval' *.googleapis.com;
+    img-src 'self' data: https: blob: https://www.googletagmanager.com;
+    worker-src 'self' *.youtube.com *.google.com *.twitter.com;
+    connect-src *;
+    object-src 'none';
+    form-action 'self';
+    frame-ancestors 'none';
+    base-uri 'none';
+    media-src 'none';
+    font-src 'self';
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+];
