5bd142efa35cd82a80ea4c18150683221d397ce1
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2022-08-13 16:24:30 +0800

    Up (#71)
    * fully mdx 2
    
    * fix some error
    
    * add gitpod button
    
    * Update README.md
    
    * Update README.md
    
    * move to .mjs
    
    * remove widget
    
    * remove and upgrade packages
    
    * revalidate
    
    * sanity deploy
    
    * sanity deploy
    
    * install
    
    * yarn
    
    * update

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,20 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { isValidRequest } from '@sanity/webhook';
+import { isValidRequest, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 import { sanityClient } from '~/lib/sanity-server';
 import { postUpdatedQuery } from '~/lib/queries';
+import { BadRequest, isValidHttpMethod, MethodNotAllowed } from '~/lib/api';
+
+async function stringifyRequest(req: NextApiRequest) {
+  const chunks = [];
+  for await (const chunk of req) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString('utf8');
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const secret = process.env.SANITY_STUDIO_REVALIDATE_SECRET;
-  if (!isValidRequest(req, secret)) {
+  if (!isValidHttpMethod(req.method, ['POST'])) {
+    return MethodNotAllowed(res);
+  }
+
+  if (!process.env.SANITY_STUDIO_REVALIDATE_SECRET) {
+    return res
+      .status(500)
+      .json({ message: 'Missing sanity studio revalidate secret' });
+  }
+
+  const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString();
+  const stringifiedRequest = await stringifyRequest(req);
+
+  if (!signature) {
+    return BadRequest(res, 'Missing signature');
+  }
+
+  if (
+    !isValidRequest(
+      stringifiedRequest,
+      signature,
+      process.env.SANITY_STUDIO_REVALIDATE_SECRET
+    )
+  ) {
     return res.status(401).json({ message: 'Invalid request' });
   }
 
-  const { _id: id } = req.body;
+  const { _id: id } = JSON.parse(stringifiedRequest);
   if (typeof id !== 'string' || !id) {
-    return res.status(400).json({ message: 'Invalid _id' });
+    return BadRequest(res, 'Invalid _id');
   }
 
   try {
@@ -23,8 +54,19 @@ export default async function handler(
       res.revalidate('/blog'),
       res.revalidate(`/blog/${slug}`)
     ]);
-    return res.status(200).json({ message: `Updated ${slug}` });
+    return res.status(200).json({ message: `[Revalidated] ${slug}` });
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    if (err instanceof Error) {
+      return res.status(500).json({ message: err.message });
+    } else {
+      return res.status(500).json({ message: 'Unknown error' });
+    }
   }
 }
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
