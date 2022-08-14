f09e7fb4c8b810d15bebc6a5a9600c9b112f56fb
Aksar La'ode <101698503+laodeaksarr@users.noreply.github.com>
2022-08-14 15:49:36 +0800

    next-auth as string

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
-import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   secret: process.env.NEXT_AUTH_SECRET,
+  theme: {
+    colorScheme: 'auto',
+    brandColor: '',
+    logo: 'https://laodeaksar.eu.org/static/images/me.png',
+  },
   providers: [
     GitHubProvider({
-      clientId: process.env.OAUTH_CLIENT_KEY,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET
-    })
-  ]
+      clientId: process.env.OAUTH_CLIENT_KEY as string,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
+    }),
+  ],
 });
