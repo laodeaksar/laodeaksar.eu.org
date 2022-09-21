94595994e5b73c2846d1cc1018388d159a1d9572
laodeaksarr <akhsarodhe@gmail.com>
2022-09-21 22:49:08 +0000

    add BASE_URL

--- pages/feed.xml.tsx
+++ pages/feed.xml.tsx
@@ -5,18 +5,20 @@ import { sanityClient } from '~/lib/sanity/sanity-server';
 import { indexQuery } from '~/lib/sanity/queries';
 import { Post } from '~/lib/types';
 
+const BASE_URL = 'https://laodeaksar.eu.org';
+
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const feed = new RSS({
     title: "Aksar La'ode",
-    site_url: 'https://laodeaksar.eu.org',
-    feed_url: 'https://laodeaksar.eu.org/feed.xml'
+    site_url: BASE_URL,
+    feed_url: `${BASE_URL}/feed.xml`
   });
 
   const allPosts = await sanityClient.fetch(indexQuery);
   allPosts.map((post: Post) => {
     feed.item({
       title: post.title,
-      url: `https://laodeaksar.eu.org/blog/${post.slug}`,
+      url: `${BASE_URL}/blog/${post.slug}`,
       date: post.date,
       description: post.description
     });
@@ -27,7 +29,11 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     'Cache-Control',
     'public, s-maxage=1200, stale-while-revalidate=600'
   );
-  res.write(feed.xml({ indent: true }));
+  res.write(
+    feed.xml({
+      indent: true
+    })
+  );
   res.end();
 
   return {
