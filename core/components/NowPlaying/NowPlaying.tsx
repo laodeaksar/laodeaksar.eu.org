6453c24fde16b1361ee23fc10ab723676a8a989d
Aksar La'ode <101698503+laodeaksarr@users.noreply.github.com>
2022-09-22 20:05:11 +0800

    Update NowPlaying.tsx

--- core/components/NowPlaying/NowPlaying.tsx
+++ core/components/NowPlaying/NowPlaying.tsx
@@ -78,7 +78,7 @@ const NowPlaying = () => {
                     color: 'var(--laodeaksar-colors-success)',
 
                     '&:hover': {
-                      display: 'block  !important'
+                      display: 'inline-flex'
                     }
                   }}
                 >
