6392a96bdd2eb86738b37469cc1fed4a51077459
bahutara <akhsaras23@gmail.com>
2022-09-27 07:37:57 +0000

    update deps

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -3,15 +3,12 @@ import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 
 import { sanityClient } from '~/lib/sanity/sanity-server';
 import { postUpdatedQuery } from '~/lib/sanity/queries';
-import { BadRequest, isValidHttpMethod, MethodNotAllowed } from '~/lib/api';
-
-async function stringifyRequest(req: NextApiRequest) {
-  const chunks = [];
-  for await (const chunk of req) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks).toString('utf8');
-}
+import {
+  BadRequest,
+  isValidHttpMethod,
+  MethodNotAllowed,
+  ServerError
+} from '~/lib/api';
 
 export default async function handler(
   req: NextApiRequest,
@@ -27,24 +24,23 @@ export default async function handler(
       .json({ message: 'Missing sanity studio revalidate secret' });
   }
 
-  const signature = req.headers[SIGNATURE_HEADER_NAME]?.toString();
-  const stringifiedRequest = await stringifyRequest(req);
-
-  if (!signature) {
-    return BadRequest(res, 'Missing signature');
-  }
-
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const body = await readBody(req); // Read the body into a string
   if (
     !isValidSignature(
-      stringifiedRequest,
+      body,
       signature,
       process.env.SANITY_STUDIO_REVALIDATE_SECRET
     )
   ) {
-    return res.status(401).json({ message: 'Invalid request' });
+    res.status(401).json({ message: 'Invalid signature' });
+    return;
   }
 
-  const { _id: id } = JSON.parse(stringifiedRequest);
+  if (!signature) {
+    return BadRequest(res, 'Missing signature');
+  }
+  const { _id: id } = JSON.parse(body);
   if (typeof id !== 'string' || !id) {
     return BadRequest(res, 'Invalid _id');
   }
@@ -57,11 +53,7 @@ export default async function handler(
     ]);
     return res.status(200).json({ message: `[Revalidated] '${slug}' (${id})` });
   } catch (err) {
-    if (err instanceof Error) {
-      return res.status(500).json({ message: err.message });
-    } else {
-      return res.status(500).json({ message: 'Unknown error' });
-    }
+    return ServerError(res, err);
   }
 }
 
@@ -71,3 +63,11 @@ export const config = {
     bodyParser: false
   }
 };
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString('utf8');
+}
