be224a183f4712a559b5c665fd6a2106cda037f4
bahutara <108091324+bahutara@users.noreply.github.com>
2022-10-03 19:09:38 +0800

    test (#92)

--- core/components/Code/types.ts
+++ core/components/Code/types.ts
@@ -18,10 +18,12 @@ export interface CodeBlockProps {
   language: Language;
   metastring: string | null;
   children?: React.ReactNode;
+  highlightLines?: Record<string, { label?: string }>;
 }
 
 export interface HighlightedCodeTextProps {
   codeString: string;
   language: Language | 'glsl';
-  highlightLine?: (index: number) => boolean;
+  //highlightLine?: (index: number) => boolean;
+  highlightLines?: Record<string, { label?: string }>;
 }
