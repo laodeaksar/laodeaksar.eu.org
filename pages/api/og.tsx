7c9c1394cb97f2e232f0986d4a53e3b2cb1576f6
aksarlaode <akhsaras21@gmail.com>
2022-10-13 13:27:09 +0000

    edit

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,8 +1,5 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import { Flex, Text } from '@bahutara/design-system';
-
-import Logo from '@/components/Logo';
 
 export const config = {
   runtime: 'experimental-edge'
@@ -25,55 +22,58 @@ export default async function handler(req: NextRequest) {
 
     return new ImageResponse(
       (
-        <Flex
-          css={{
-            color: query.color,
+        <div
+          style={{
+            display: 'flex',
             padding: 60,
             backgroundImage: `linear-gradient(${query.bgOverlay}, ${query.bgOverlay}), url(${query.bgImage})`,
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat'
           }}
         >
-          <Flex
-            alignItems="flex-end"
-            justifyContent="space-evenly"
-            css={{
-              size: '$full'
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'flex-end',
+              justifyContent: 'space-between',
+              height: '100%',
+              width: '100%'
             }}
           >
-            <Flex
-              direction="column"
-              css={{
-                height: '$full',
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
                 flex: '1 0 50%'
               }}
             >
-              <Text
-                css={{
+              <div
+                style={{
                   fontFamily: 'IBM Plex Mono',
                   fontSize: 16,
                   fontWeight: 400,
                   opacity: 0.7,
-                  mb: 3
+                  marginBottom: 3
                 }}
               >
                 {date}
                 {' — '}
                 {readTime}
-              </Text>
-              <Text
-                css={{
-                  fontFamily: '$display',
+              </div>
+              <div
+                style={{
+                  fontFamily: 'Inter',
                   fontSize: 40,
                   fontWeight: 800,
                   lineHeight: 1.4
                 }}
               >
                 {title}
-              </Text>
-              <Text
-                css={{
-                  fontFamily: '$mono',
+              </div>
+              <div
+                style={{
+                  fontFamily: 'IBM Plex Mono',
                   fontSize: 16,
                   fontWeight: 400,
                   opacity: 0.7,
@@ -81,11 +81,35 @@ export default async function handler(req: NextRequest) {
                 }}
               >
                 {query.domain}
-              </Text>
-            </Flex>
-            <Logo />
-          </Flex>
-        </Flex>
+              </div>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 595 503"
+              xmlSpace="preserve"
+              aria-label="Aksar La'ode"
+              width={44}
+              fill="none"
+              stroke="#000"
+            >
+              <path
+                d="M296.6 44.1c46.9 0 85 38.1 85 85v250.5c0 46.9-38.1 85-85 85s-85-38.1-85-85V129.1c0-47 38-85 85-85z"
+                strokeMiterlimit={10}
+                strokeWidth={30}
+              />
+              <path
+                d="M367.8 152.5 218.5 414.7c-22.4 39.4-74.9 58.2-117.2 42-42.3-16.2-58.4-61.2-36-100.5L214.6 94c22.4-39.4 74.9-58.2 117.2-42s58.4 61.1 36 100.5z"
+                strokeMiterlimit={10}
+                strokeWidth={40}
+              />
+              <path
+                d="M266.4 52c42.3-16.2 94.8 2.6 117.2 42l149.3 262.2c22.4 39.4 6.3 84.4-36 100.5-42.3 16.2-94.8-2.6-117.2-42L230.3 152.5C207.9 113.1 224 68.1 266.4 52z"
+                strokeMiterlimit={10}
+                strokeWidth={40}
+              />
+            </svg>
+          </div>
+        </div>
       ),
       {
         width: 1200,
