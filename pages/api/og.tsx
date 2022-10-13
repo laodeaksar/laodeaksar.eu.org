f8eb5232554957c09a58a4d7ed1c833085c6340b
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2022-10-14 00:08:28 +0800

    Update og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -130,3 +130,69 @@ const query = {
   bgOverlay: 'rgba(255,255,255,0.7)',
   domain: 'laodeaksar.eu.org'
 };
+
+<div
+      style={{
+        display: 'flex',
+        color: '#000',
+        width: '100%',
+        height: '100%',
+        padding: 60,
+        backgroundImage: `linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(https://images.unsplash.com/photo-1558591710-4b4a1ae0f04d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'flex-end',
+          justifyContent: 'space-between',
+          height: '100%',
+          width: '100%'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            flex: '1 0 50%'
+          }}
+        >
+          <span
+            style={{
+              fontFamily: 'IBM Plex Mono',
+              fontSize: 16,
+              fontWeight: 400,
+              opacity: 0.7,
+              marginBottom: 3
+            }}
+          >
+            query.subtitle
+          </span>
+          <span
+            style={{
+              fontFamily: 'Inter',
+              fontSize: 40,
+              fontWeight: 800,
+              lineHeight: 1.4
+            }}
+          >
+            query.title
+          </span>
+          <span
+            style={{
+              fontFamily: 'IBM Plex Mono',
+              fontSize: 16,
+              fontWeight: 400,
+              opacity: 0.7,
+              marginTop: 'auto'
+            }}
+          >
+            query.domain
+          </span>
+        </div>
+        s
+      </div>
+    </div>
